5902896280ee182390c84d30585db205c3189640
疯狂减肥带 <ureygt@gmail.com>
2016-07-09 12:03:19 +0800

    changer

--- json.php
+++ json.php
@@ -8,5 +8,8 @@ curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 $contents = curl_exec($ch); 
 curl_close($ch); 
 header('Content-Type: text/json;charset=utf-8');
+header('Access-Control-Allow-Origin:*.haotown.cn');  
+header('Access-Control-Allow-Methods:POST');  
+header('Access-Control-Allow-Headers:x-requested-with,content-type'); 
 echo $contents; 
 ?> 
